88625b4f4e548e5453623cfa3473681797b19d76
Patrick Bentley <pbentley07@gmail.com>
2016-07-05 21:51:07 -0400

    Added Docs Views and Defined Actions

--- cabinet/app/controllers/docs_controller.rb
+++ cabinet/app/controllers/docs_controller.rb
@@ -1,10 +1,12 @@
 class DocsController < ApplicationController
 
-
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
   def index
+    @docs = Doc.all.order('created_at DESC')
   end
 
   def show
+
   end
 
   def destroy
@@ -14,9 +16,18 @@ class DocsController < ApplicationController
   end
 
   def new
+    @doc = Doc.new
   end
 
   def create
+    @doc = Doc.new(doc_params)
+
+    if @doc.save
+      redirect_to @doc
+
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -28,9 +39,11 @@ class DocsController < ApplicationController
   private
 
   def find_doc
+    @doc = Doc.find(params[:id])
   end
 
   def doc_params
+    params.require(:doc).permit(:title, :content)
   end
-  
+
 end
